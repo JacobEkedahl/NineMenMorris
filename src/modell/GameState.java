395c53685d362945f3c9e618a7053a0d8f89eace
JacobEkedahl <jacob.ekedahl@gmail.com>
2017-10-15 20:51:18 +0200

    Preparing for merge, made a list of imageview controllers on game which changes backgroundimage on hover

--- src/modell/GameState.java
+++ src/modell/GameState.java
@@ -3,13 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package view;
+package modell;
 
 /**
  *
  * @author Jacob
  */
-public class LayoutCSS {
-
-
+public enum GameState {
+    PLACING, MIDDLE, FLYING;
 }
--- src/view/NineMenMorris.java
+++ src/view/NineMenMorris.java
@@ -7,6 +7,8 @@ package view;
  */
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
@@ -25,6 +27,7 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.BackgroundPosition;
@@ -49,6 +52,13 @@ import modell.Game;
  */
 public class NineMenMorris extends Application {
 
+    //Setting limits on what user can do
+    private boolean isPositionClickable;
+    private boolean isPlayerOnePiecesClickable;
+    private boolean isPlayerTwoPiecesClickable;
+    private boolean isPositionHoverable;
+
+    private boolean gameRunning;
     private BorderPane mainPane;
     private GridPane whitePlayerPane;
 
@@ -57,11 +67,18 @@ public class NineMenMorris extends Application {
 
     private ArrayList<ImageView> whitePieces;
     private ArrayList<ImageView> blackPieces;
+    private ArrayList<ImageView> positionImages;
 
-    private Game gameSession;
+    private static Game gameSession;
 
     @Override
     public void start(Stage primaryStage) {
+        //initiate gamevariables
+        gameRunning = false;
+        isPositionClickable = false;
+        isPlayerOnePiecesClickable = false;
+        isPlayerTwoPiecesClickable = false;
+        isPositionHoverable = false;
 
         //Menu
         MenuBar menuBar = new MenuBar();
@@ -79,29 +96,108 @@ public class NineMenMorris extends Application {
 
         //Connecting items to eventhandler
         newGameHandles newGame = new newGameHandles();
+
         restartGameItem.setOnAction(newGame);
-        
+
         itemAboutHandles itemAbout = new itemAboutHandles();
+
         aboutItem.setOnAction(itemAbout);
         itemRulesHandles itemRules = new itemRulesHandles();
+
         rulesItem.setOnAction(itemRules);
 
         //Connect Menu and items
-        fileMenu.getItems().addAll(openItem, saveItem);
-        gameMenu.getItems().addAll(restartGameItem, regretMoveItem);
-        helpMenu.getItems().addAll(aboutItem, rulesItem);
-        menuBar.getMenus().addAll(fileMenu, gameMenu, helpMenu);
+        fileMenu.getItems()
+                .addAll(openItem, saveItem);
+        gameMenu.getItems()
+                .addAll(restartGameItem, regretMoveItem);
+        helpMenu.getItems()
+                .addAll(aboutItem, rulesItem);
+        menuBar.getMenus()
+                .addAll(fileMenu, gameMenu, helpMenu);
+
+        //Gameboard positions
+        File filePosition = new File("src/Images/transparentPos.png");
+        Image positionImage = new Image(filePosition.toURI().toString());
+        Group groupOfPos = new Group();
+
+        positionImages = new ArrayList<ImageView>();
+        int positionIndex = 0;
+
+        for (int j = 0;
+                j < 3; j++) { //outer quadrant
+            for (int i = 0; i < 3; i++) { //24 is number of positions
+                if (!(i == 1 && j == 1)) {
+                    ImageView positionView = new ImageView(positionImage);
+                    positionView.setId("" + positionIndex);
+                    positionIndex++;
+                    positionView.setFitHeight(60); //set size of pieces
+                    positionView.setFitWidth(60);
+                    positionView.setPreserveRatio(true);
+                    positionView.setSmooth(true);
+                    positionView.setLayoutX(-8 + (i * 265));
+                    positionView.setLayoutY(25 + (j * 265));
+                    positionView.addEventHandler(MouseEvent.MOUSE_ENTERED, new positionEnter());
+                    positionView.addEventHandler(MouseEvent.MOUSE_EXITED, new positionExit());
+                    positionImages.add(positionView);
+                    groupOfPos.getChildren().add(positionView);
+                }
+            }
+        }
+
+        for (int j = 0;
+                j < 3; j++) { //middle quadrant
+            for (int i = 0; i < 3; i++) { //24 is number of positions
+                if (!(i == 1 && j == 1)) {
+                    ImageView positionView = new ImageView(positionImage);
+                    positionView.setId("" + positionIndex);
+                    positionIndex++;
+                    positionView.setFitHeight(60); //set size of pieces
+                    positionView.setFitWidth(60);
+                    positionView.setPreserveRatio(true);
+                    positionView.setSmooth(true);
+                    positionView.setLayoutX(77 + (i * 180));
+                    positionView.setLayoutY(110 + (j * 180));
+                    positionView.addEventHandler(MouseEvent.MOUSE_ENTERED, new positionEnter());
+                    positionView.addEventHandler(MouseEvent.MOUSE_EXITED, new positionExit());
+                    positionImages.add(positionView);
+                    groupOfPos.getChildren().add(positionView);
+                }
+            }
+        }
+
+        for (int j = 0;
+                j < 3; j++) { //inner quadrant
+            for (int i = 0; i < 3; i++) { //24 is number of positions
+                if (!(i == 1 && j == 1)) {
+                    ImageView positionView = new ImageView(positionImage);
+                    positionView.setId("" + positionIndex);
+                    positionIndex++;
+                    positionView.setFitHeight(60); //set size of pieces
+                    positionView.setFitWidth(60);
+                    positionView.setPreserveRatio(true);
+                    positionView.setSmooth(true);
+                    positionView.setLayoutX(157 + (i * 100));
+                    positionView.setLayoutY(190 + (j * 100));
+                    positionView.addEventHandler(MouseEvent.MOUSE_ENTERED, new positionEnter());
+                    positionView.addEventHandler(MouseEvent.MOUSE_EXITED, new positionExit());
+                    positionImages.add(positionView);
+                    groupOfPos.getChildren().add(positionView);
+                }
+            }
+        }
 
         //Pieces and players name
-        File file = new File("src/view/whiteCircle.png");
+        File file = new File("src/Images/whiteCircle.png");
         Image imageWhite = new Image(file.toURI().toString());
-        file = new File("src/view/blackCircle.png");
+        file = new File("src/Images/blackCircle.png");
         Image imageBlack = new Image(file.toURI().toString());
 
         whitePieces = new ArrayList<ImageView>();
         blackPieces = new ArrayList<ImageView>();
 
-        for (int i = 0; i < 9; i++) {
+        for (int i = 0;
+                i < 9; i++) {
             ImageView whitePiece = new ImageView(imageWhite);
             ImageView blackPiece = new ImageView(imageBlack);
             whitePiece.setFitHeight(50); //set size of pieces
@@ -118,34 +214,55 @@ public class NineMenMorris extends Application {
         }
 
         GridPane grid = new GridPane();
-        grid.setHgap(10); //horizontal gap in pixels => that's what you are asking for
-        grid.setVgap(10); //vertical gap in pixels
-        grid.setPadding(new Insets(10, 10, 10, 10));
+
+        grid.setHgap(
+                10); //horizontal gap in pixels => that's what you are asking for
+        grid.setVgap(
+                10); //vertical gap in pixels
+        grid.setPadding(
+                new Insets(10, 10, 10, 10));
 
         playerOneLbl = new Label("Player 1");
         playerTwoLbl = new Label("Player 2");
 
-        addPiecesToGrid(grid, whitePieces, 2);
-        grid.add(playerOneLbl, 0, 1, 4, 1); //spans over hole row
-        addPiecesToGrid(grid, blackPieces, 18);
-        grid.add(playerTwoLbl, 0, 17, 4, 1);
+        addPiecesToGrid(grid, whitePieces,
+                2);
+        grid.add(playerOneLbl,
+                0, 1, 4, 1); //spans over hole row
+        addPiecesToGrid(grid, blackPieces,
+                18);
+        grid.add(playerTwoLbl,
+                0, 17, 4, 1);
 
         // FlowPane bottomPane = new FlowPane();
         // bottomPane.getChildren().addAll(newGamebtn, regretLastMovebtn);
         //Create a game players and get their names and position..
         //mainPane.setStyle("-fx-background-color: Black");
         mainPane = new BorderPane();
-        File fileBack = new File("src/view/backgroundImage.jpg");
+        File fileBack = new File("src/Images/backgroundImage.jpg");
+
         loadBackgroundImage(mainPane, fileBack);
+
         mainPane.setTop(menuBar);
+
         mainPane.setRight(grid);
+
+        mainPane.getChildren()
+                .add(groupOfPos);
 
         Scene scene = new Scene(mainPane, 805, 590);
 
-        primaryStage.setTitle("Nine Men Morris!");
+        primaryStage.setTitle(
+                "Nine Men Morris!");
         primaryStage.setScene(scene);
-        primaryStage.setResizable(false);
+
+        primaryStage.setResizable(
+                false);
         primaryStage.show();
+    }
+
+    private void addPositionsToBoard(BorderPane mainPane) {
+
     }
 
     private void addPiecesToGrid(GridPane grid, ArrayList<ImageView> pieces, int startY) {
@@ -167,20 +284,62 @@ public class NineMenMorris extends Application {
                 BackgroundRepeat.NO_REPEAT,
                 BackgroundPosition.CENTER,
                 bSize)));
+
     }
-    
+
+    private class positionExit implements EventHandler<MouseEvent> {
+
+        @Override
+        public void handle(MouseEvent event) {
+            ImageView targetView = (ImageView) event.getSource();
+            for (int i = 0; i < positionImages.size(); i++) {
+                if (targetView.getId().equals(positionImages.get(i).getId())) {
+                    File filePosition = new File("src/Images/transparentPos.png");
+                    Image positionImage = new Image(filePosition.toURI().toString());
+                    positionImages.get(i).setImage(positionImage);
+
+                }
+            }
+            System.out.println(targetView.getId());
+        }
+
+    }
+
+    private class positionEnter implements EventHandler<MouseEvent> {
+
+        @Override
+        public void handle(MouseEvent event) {
+            ImageView targetView = (ImageView) event.getSource();
+            for (int i = 0; i < positionImages.size(); i++) {
+                if (targetView.getId().equals(positionImages.get(i).getId())) {
+                    File filePosition = new File("src/Images/hoverPosition.png");
+                    Image positionImage = new Image(filePosition.toURI().toString());
+
+                    positionImages.get(i).setImage(positionImage);
+
+                }
+            }
+            System.out.println(targetView.getId());
+        }
+
+    }
+
     private class newGameHandles implements EventHandler<ActionEvent> {
 
         @Override
         public void handle(ActionEvent event) {
-            
+            if (gameRunning == true) {
+                gameRunning = false;
+                //stop current thread then set value to false
+            } else {
+            }
             //start gamethread
-           /* while (true) {
+            /* while (true) {
                 System.out.println("gameloop");
             }
-            */
+             */
         }
-        
+
     }
 
     private class itemAboutHandles implements EventHandler<ActionEvent> {
@@ -191,7 +350,7 @@ public class NineMenMorris extends Application {
 
             //Button btn = new Button("Hello");
             //backGroundPane.getChildren().add(btn);
-            File fileBack = new File("src/view/backgroundAbout.jpg");
+            File fileBack = new File("src/Images/backgroundAbout.jpg");
             loadBackgroundImage(backGroundPane, fileBack);
 
             Text text = new Text("This game was created by Jacob Ekedahl and Tobias Degnell.");
@@ -215,7 +374,7 @@ public class NineMenMorris extends Application {
 
             //Button btn = new Button("Hello");
             //backGroundPane.getChildren().add(btn);
-            File fileBack = new File("src/view/backgroundAbout.jpg");
+            File fileBack = new File("src/Images/backgroundAbout.jpg");
             loadBackgroundImage(backGroundPane, fileBack);
 
             Text text = new Text("Nine Men's Morris is a very old game that has been played for thousands of years all over the world. It was most \n"
